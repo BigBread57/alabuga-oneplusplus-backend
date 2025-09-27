f7938eaca0f23d2c08ea01d3f471c9c984994c28
gleb <mag@letsnova.ru>
2025-09-27 17:17:32 +0300

    fix: исправлен импорт defaultdict с mypy на collections

--- app/game_world/api/v1/services/game_world.py
+++ app/game_world/api/v1/services/game_world.py
@@ -1,6 +1,6 @@
+from collections import defaultdict
 from typing import Any
 
-from mypy.checkexpr import defaultdict
 
 from common.services import BaseService
 from game_world.models import GameWorld
